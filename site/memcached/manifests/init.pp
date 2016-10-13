6307704a678ecd2c3e7a495b92cba30f7c7cfe49
Dzmitry Vadalazhski <dv@dv.com>
2016-10-13 11:51:15 -0400

    memcached class

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,17 @@
+class memcached {
+  package { 'memcached':
+	ensure => present,  
+  }
+  
+  file {'/etc/sysconfig/memcached'
+	require => Package['memcached'],
+	ensure => file,
+	source => 'puppet:///modules/memcached/memcached',
+  }
+  
+  service {'memcached':
+	ensure => running,
+	enable => true,
+	subscribe => File['/etc/sysconfig/memcached'],
+  }
+}
