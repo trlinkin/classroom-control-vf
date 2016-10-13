584cea8b014562b1059fc798de9e00bccc40304e
Dzmitry Vadalazhski <dv@dv.com>
2016-10-13 14:19:31 -0400

    changed enabled to 1 in yumrepo 'updates'

--- site/repo/manifests/init.pp
+++ site/repo/manifests/init.pp
@@ -2,7 +2,7 @@ class repo {
   yumrepo { 'updates':
     ensure     => 'present',
     descr      => 'CentOS-$releasever - Updates',
-    enabled    => '0',
+    enabled    => '1',
     gpgcheck   => '1',
     gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
     mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
